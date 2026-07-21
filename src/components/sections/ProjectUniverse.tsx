de0806620bc8464aa1ec48d0bbd10c4388a5d979
Kanishk Karam <156420103+Kanishkkaram2703@users.noreply.github.com>
2026-07-22 00:10:16 +0530

    Refactor bufferAttribute props to use args array

--- src/components/sections/ProjectUniverse.tsx
+++ src/components/sections/ProjectUniverse.tsx
@@ -113,8 +113,7 @@ function StarField() {
         <bufferAttribute
           attach="attributes-position"
           count={count}
-          array={positions}
-          itemSize={3}
+          args={[positions, 3]}
         />
       </bufferGeometry>
       <pointsMaterial size={0.05} color="#ffffff" transparent opacity={0.6} sizeAttenuation />
